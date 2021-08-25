3547a919fb95fb7043ab2723608c4230015a67fc
sharmaRajat13 <sharmarajat.1308@gmail.com>
2021-08-25 17:28:42 +0530

    added new code

--- src/main/java/com/example/springsecurityexample/department/controller/CategoryResource.java
+++ src/main/java/com/example/springsecurityexample/department/controller/CategoryResource.java
@@ -1,7 +1,21 @@
 package com.example.springsecurityexample.department.controller;
 
+import com.example.springsecurityexample.department.model.Category;
+import com.example.springsecurityexample.department.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 public class CategoryResource {
+
+    private final CategoryService categoryService;
+
+    @GetMapping(path = "/getByCatName/{catName}")
+    public ResponseEntity<Category> findByCatName(@RequestParam String catName){
+        return ResponseEntity.ok(categoryService.findByCatName(catName));
+    }
 }
--- src/main/java/com/example/springsecurityexample/department/service/CategoryService.java
+++ src/main/java/com/example/springsecurityexample/department/service/CategoryService.java
@@ -1,7 +1,19 @@
 package com.example.springsecurityexample.department.service;
 
+import com.example.springsecurityexample.department.model.Category;
+import com.example.springsecurityexample.department.repository.CategoryRepo;
+import lombok.RequiredArgsConstructor;
 import org.jvnet.hk2.annotations.Service;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
+@RequiredArgsConstructor
 public class CategoryService {
+
+    private final CategoryRepo categoryRepo;
+
+    public Category findByCatName(String catName){
+       return categoryRepo.findByCatName(catName).orElseThrow( () -> new EntityNotFoundException("please give correct category name"));
+    }
 }
